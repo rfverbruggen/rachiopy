d6737d2dbec035d16fcc8919b816dc383f3ed964
Robbert Verbruggen <rfverbruggen@me.com>
2015-09-17 15:57:42 +0200

    Update schedulerule.py
    json.dumps payload and return (resp, content)

--- rachiopy/schedulerule.py
+++ rachiopy/schedulerule.py
@@ -1,31 +1,33 @@
+import json 
+
 class Schedulerule(object):
 	def __init__(self, rachio):
 		self.rachio = rachio
 
 	def skip(self, id):
 		url = '%sschedulerule/skip' % self.rachio.server
-		body = { 'id' : id }
+		payload = { 'id' : id }
 
-		(resp, content) = self.rachio.h.request(url, 'PUT', body=body, header=self.rachio.headers)
-		return content
+		(resp, content) = self.rachio.h.request(url, 'PUT', body=json.dumps(payload), header=self.rachio.headers)
+		return (resp, content)
 
 	def start(self, id):
 		url = '%sschedulerule/start' % self.rachio.server
-		body = { 'id': id }
+		payload = { 'id': id }
 
-		(resp, content) = self.rachio.h.request(url, 'PUT', body=body, headers=self.rachio.headers)
-		return content
+		(resp, content) = self.rachio.h.request(url, 'PUT', body=json.dumps(payload), headers=self.rachio.headers)
+		return (resp, content)
 
 	def seasonalAdjustment(self, id, adjustment):
 		url = '%sschedulerule/seasonal_adjustment' % self.rachio.server
-		body = { 'id': id, 'adjustment': adjustment }
+		payload = { 'id': id, 'adjustment': adjustment }
 
-		(resp, content) = self.rachio.h.request(url, 'PUT', body=body, headers=self.rachio.headers)
-		return content
+		(resp, content) = self.rachio.h.request(url, 'PUT', body=json.dumps(payload), headers=self.rachio.headers)
+		return (resp, content)
 
 	def get(self, id):
 		url = '%sschedulerule/%s' % (self.rachio.server, id)
 
 		(resp, content) = self.rachio.h.request(url, 'GET', headers=self.rachio.headers)
-		return content
+		return (resp, content)
 
