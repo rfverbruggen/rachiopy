f3baf0fe8767b3c28ffcaed2395055a839b691fb
Robbert Verbruggen <rfverbruggen@me.com>
2015-09-17 15:47:37 +0200

    Update zone.py
    import json and json.dump payloads before putting it into the request data argument

--- rachiopy/zone.py
+++ rachiopy/zone.py
@@ -1,23 +1,25 @@
+import json
+
 class Zone(object):
     def __init__(self, rachio):
         self.rachio = rachio
 
     def start(self, id, duration):
         url = '%szone/start' % self.rachio.server
-        body = {'id': id, 'duration': duration}
+        payload = {'id': id, 'duration': duration}
 
-        (resp, content) = self.rachio.h.request(url, 'PUT', body=body, headers=self.rachio.headers)
-        return content
+        (resp, content) = self.rachio.h.request(url, 'PUT', body=json.dumps(payload), headers=self.rachio.headers)
+        return (resp, content)
 
     def startMultiple(self, zones):
         url = '%szone/start_multiple' % self.rachio.server
-        body = {'zones': zones}
+        payload = {'zones': zones}
 
-        (resp, content) = self.rachio.h.request(url, 'PUT', headers=self.rachio.headers)
-        return content
+        (resp, content) = self.rachio.h.request(url, 'PUT', body=json.dumps(payload), headers=self.rachio.headers)
+        return (resp, content)
 
     def get(self, id):
         url = '%szone/%s' % (self.rachio.server, id)
 
         (resp, content) = self.rachio.h.request(url, 'GET', headers=self.rachio.headers)
-        return content
+        return (resp, content)
