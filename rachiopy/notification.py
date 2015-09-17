fe376418b9d502381b4ac9f69a4465fe50ddc843
Robbert Verbruggen <rfverbruggen@me.com>
2015-09-17 15:55:03 +0200

    Update notification.py
    json.dumps of payload and return (resp, content)

--- rachiopy/notification.py
+++ rachiopy/notification.py
@@ -1,3 +1,5 @@
+import json
+
 class Notification(object):
 	def __init__(self, rachio):
 		self.rachio = rachio
@@ -6,36 +8,36 @@ class Notification(object):
 		url = '%snotification/webhook_event_type' % self.rachio.server
 
 		(resp, content) = self.rachio.h.request(url, 'GET', headers=self.rachio.headers)
-		return content
+		return (resp, content)
 
 	def getDeviceWebhook(self, id):
 		url = '%snotification/%s/webhook' % (self.rachio.server, id)
 
 		(resp, content) = self.rachio.h.request(url, 'GET', headers=self.rachio.headers)
-		return content
+		return (resp, content)
 
 	def postWebhook(self, webhook):
 		url = '%snotification/webhook' % self.rachio.server
-		body = {webhook}
+		payload = {webhook}
 
-		(resp, content) = self.rachio.h.request(url, 'POST', body=body, headers=self.rachio.headers)
-		return content
+		(resp, content) = self.rachio.h.request(url, 'POST', body=json.dumps(payload), headers=self.rachio.headers)
+		return (resp, content)
 
 	def putWebhook(self, webhook):
 		url = '%snotification/webhook' % self.rachio.server
-		body = {webhook}
+		payload = {webhook}
 
-		(resp, content) = self.rachio.h.request(url, 'PUT', body=body, headers=self.rachio.headers)
-		return content
+		(resp, content) = self.rachio.h.request(url, 'PUT', body=json.dumps(payload), headers=self.rachio.headers)
+		return (resp, content)
 
 	def deleteWebhook(self, id):
 		url = '%snotification/webhook/%s' % (self.rachio.server, id)
 
 		(resp, content) = self.rachio.h.request(url, 'DELETE', headers=self.rachio.headers)
-		return content
+		return (resp, content)
 
 	def get(self, id):
 		url = '%snotification/webhook/%s' % (self.rachio.server, id)
 
 		(resp, content) = self.rachio.h.request(url, 'GET', headers=self.rachio.headers)
-		return content
+		return (resp, content)
