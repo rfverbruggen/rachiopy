f4f2d90a129e2d4d464737d55a24a288530be65b
Robbert Verbruggen <rfverbruggen@me.com>
2015-09-17 15:59:32 +0200

    Update device.py
    json.dumps payload and return (resp, content)

--- rachiopy/device.py
+++ rachiopy/device.py
@@ -1,3 +1,5 @@
+import json
+
 class Device(object):
 	def __init__(self, rachio):
 		self.rachio = rachio
@@ -6,56 +8,56 @@ class Device(object):
 		url = '%sdevice/%s' % (self.rachio.server, id)
 
 		(resp, content) = self.rachio.h.request(url, 'GET', headers=self.rachio.headers)
-		return content
+		return (resp, content)
 
 	def getCurrentSchedule(self, id):
 		url = '%sdevice/%s/current_schedule' % (self.rachio.server, id)
 
 		(resp, content) = self.rachio.h.request(url, 'GET', headers=self.rachio.headers)
-		return content
+		return (resp, content)
 
 	def getEvent(self, id, starttime, endtime):
 		url = '%sdevice/%s/event?startTime=%s&endTime=%s' % (self.rachio.server, id, starttime, endtime)
 
 		(resp, content) = self.rachio.h.request(url, 'GET', headers=self.rachio.headers)
-		return content
+		return (resp, content)
 
 	def getScheduleItem(self, id):
 		url = '%sdevice/%s/scheduleitem' % (self.rachio.server, id)
 
 		(resp, content) = self.rachio.h.request(url, 'GET', headers=self.rachio.headers)
-		return content
+		return (resp, content)
 
 	def getForecast(self, id, units):
 		url = '%s%sdevice/%s/forecast?units=%s' % (self.rachio.server, id, units)
 
 		(resp, content) = self.rachio.h.request(url, 'GET', headers=self.rachio.headers)
-		return content
+		return (resp, content)
 
 	def stopWater(self, id):
 		url = '%sdevice/stop_water' % self.rachio.server
-		body = {'id' : id } 
+		payload = {'id' : id } 
 
-		(resp, content) = self.rachio.h.request(url, 'PUT', body=body, headers=self.rachio.headers)
-		return content
+		(resp, content) = self.rachio.h.request(url, 'PUT', body=json.dumps(payload), headers=self.rachio.headers)
+		return (resp, content)
 
 	def rainDelay(self, id, duration):
 		url = '%sdevice/rain_delay' % self.rachio.server
-		body = { 'id' : id, 'duration' : duration }
+		payload = { 'id' : id, 'duration' : duration }
 
-		(resp, content) = self.rachio.h.request(url, 'PUT', body=body, headers=self.rachio.headers)
-		return content
+		(resp, content) = self.rachio.h.request(url, 'PUT', body=json.dumps(payload), headers=self.rachio.headers)
+		return (resp, content)
 
 	def on(self, id):
 		url = '%sdevice/on' % self.rachio.server
-		body = { 'id' : id }
+		payload = { 'id' : id }
 
-		(resp, content) = self.rachio.h.request(url, 'PUT', body=body, headers=self.rachio.headers)
-		return content
+		(resp, content) = self.rachio.h.request(url, 'PUT', body=json.dumps(payload), headers=self.rachio.headers)
+		return (resp, content)
 
 	def off(self, id):
 		url = '%sdevice/off' % self.rachio.server
-		body = { 'id' : id }
+		payload = { 'id' : id }
 
-		(resp, content) = self.rachio.h.request(url, 'PUT', body=body, header=self.rachio.headers)
-		return content 
+		(resp, content) = self.rachio.h.request(url, 'PUT', body=json.dumps(payload), header=self.rachio.headers)
+		return (resp, content) 
