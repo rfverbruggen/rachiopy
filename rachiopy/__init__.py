00db7d11d72e5aea0099e0d0a5f71e765766fde9
Ryan Kraus <rmkraus@gmail.com>
2017-04-08 18:23:32 -0400

    Changed __init__.py to use full imports
    Relative imports do not work the same way in Python 3 as they did in
    Python 2. Full imports ensure compatibility between versions.

--- rachiopy/__init__.py
+++ rachiopy/__init__.py
@@ -1,11 +1,11 @@
 import httplib2
 
-from person import Person
-from device import Device
-from zone import Zone
-from schedulerule import Schedulerule
-from flexschedulerule import FlexSchedulerule
-from notification import Notification
+from rachiopy.person import Person
+from rachiopy.device import Device
+from rachiopy.zone import Zone
+from rachiopy.schedulerule import Schedulerule
+from rachiopy.flexschedulerule import FlexSchedulerule
+from rachiopy.notification import Notification
 
 class Rachio(object):
 	def __init__(self, authtoken):
