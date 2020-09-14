14036ae3e50619294bb703361b654fd20b2bdf2d
J. Nick Koston <nick@koston.org>
2020-09-14 10:46:34 -0500

    Convert to using requests to resolve ssl errors

--- rachiopy/__init__.py
+++ rachiopy/__init__.py
@@ -1,7 +1,6 @@
 """Main rachiopy module."""
 
 import json
-import httplib2
 
 from rachiopy.person import Person
 from rachiopy.device import Device
@@ -10,14 +9,15 @@ from rachiopy.schedulerule import Schedulerule
 from rachiopy.flexschedulerule import FlexSchedulerule
 from rachiopy.notification import Notification
 
-_SERVER = 'https://api.rach.io/1/public'
-_HTTP = httplib2.Http()
+from requests import Session
+
+_SERVER = "https://api.rach.io/1/public"
 
 
 class Rachio(object):
     """Represent the Rachio API."""
 
-    def __init__(self, authtoken):
+    def __init__(self, authtoken, http_session=None):
         """Rachio class initializer."""
         self._headers = {'Content-Type': 'application/json',
                          'Authorization': 'Bearer %s' % authtoken}
@@ -28,18 +28,23 @@ class Rachio(object):
         self.schedulerule = Schedulerule(self)
         self.flexschedulerule = FlexSchedulerule(self)
         self.notification = Notification(self)
+        self._http_session = http_session or Session()
 
     def _request(self, path, method, body=None):
         """Make a request from the API."""
-        url = '/'.join([_SERVER, path])
-        (resp, content) = _HTTP.request(url, method,
-                                        headers=self._headers, body=body)
+        url = "/".join([_SERVER, path])
+        response = self._http_session.request(
+            method, url, headers=self._headers, data=body
+        )
 
-        content_type = resp.get('content-type')
-        if content_type and content_type.startswith('application/json'):
-            content = json.loads(content.decode('UTF-8'))
+        content_type = response.headers.get("content-type")
+        headers = {k.lower(): v for k, v in response.headers.items()}
+        headers["status"] = response.status_code
 
-        return (resp, content)
+        if content_type and content_type.startswith("application/json"):
+            return headers, response.json()
+
+        return headers, response.text
 
     def get(self, path):
         """Make a GET request from the API."""
